b5c655889bc1e6b40b01ad0bdf12bbb5eca7b2ca
Wan Zulkarnain <wan@wanzul-hosting.com>
2017-12-12 15:00:32 +0800

    Test

---
+++ src/main/java/com/team2/compilerepo/CompileCode.java
@@ -0,0 +1,14 @@
+package com.team2.compilerepo;
+
+public class CompileCode implements Runnable {
+
+    public static void compileNow() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+}
